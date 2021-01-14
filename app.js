cf785ca1ccf12e2bbee6c0fd02295abd7e8ea9d0
Wishtrader <mr.akomarov@gmail.com>
2021-01-14 08:57:30 +0300

    Start new Layout

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const Post = require('./models/post');
 
@@ -7,6 +8,11 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  '/js',
+  express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist'))
+);
 
 app.get('/', (req, res) => {
   Post.find({}).then(posts => {
